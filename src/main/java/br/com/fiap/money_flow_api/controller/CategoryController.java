7170dfaaa5931b1e62d95369a521a338e9f1c986
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2025-03-10 22:48:41 -0300

    crud completo refatorado

--- src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
@@ -2,53 +2,66 @@ package br.com.fiap.money_flow_api.controller;
 
 
 import br.com.fiap.money_flow_api.model.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
     private List<Category> repository = new ArrayList<>();
 
-    // GET - listar todas as categorias
     @GetMapping()
     public List<Category> index() {
         return repository;
     }
 
-    // GET - buscar por ID
     @GetMapping("/{id}")
-    public ResponseEntity<Category> getById(@PathVariable Long id) {
-        System.out.println("Buscando categoria: " + id);
-
-        var category = repository
-                .stream()
-                .filter(c -> c.getId().equals(id))
-                .findFirst();
-
-        if (category.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(category.get());
+    public Category getById(@PathVariable Long id) {
+        return getCategory(id); // 200
     }
 
-    // POST - criar categoria
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Category> create(@RequestBody Category category) {
-        System.out.println("Cadastrando..." + category.getName());
+        log.info("Cadastrando..." + category.getName());
         repository.add(category);
         return ResponseEntity.status(201).body(category);
     }
 
-    // DELETE - apagar categoria
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        log.info("Apagando categoria: " + id);
+        repository.remove(getCategory(id));
+    }
 
-    // EDITAR - editar categoria
+    @PutMapping("/{id}")
+    public Category update(@PathVariable Long id, @RequestBody Category category) {
+        log.info("Atualizando categoria " + id + " " + category);
+
+        repository.remove(getCategory(id));
+        category.setId(id);
+        repository.add(category);
+
+        return category;
+    }
+
+    private Category getCategory(Long id) {
+        return repository
+            .stream()
+            .filter(c -> c.getId().equals(id))
+            .findFirst()
+            .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+            );
+    }
 }
--- src/main/java/br/com/fiap/money_flow_api/model/Category.java
+++ src/main/java/br/com/fiap/money_flow_api/model/Category.java
@@ -8,9 +8,13 @@ public class Category {
     private String icon;
 
     public Category(Long id, String name, String icon) {
-        this.id = (id == null) ? new Random().nextLong() : id;
+        this.id = (id == null) ? Math.abs(new Random().nextLong()) : id;
         this.name = name;
         this.icon = icon;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getId() {
@@ -23,5 +27,10 @@ public class Category {
 
     public String getIcon() {
         return icon;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", icon=" + icon + "]";
     }
 }
