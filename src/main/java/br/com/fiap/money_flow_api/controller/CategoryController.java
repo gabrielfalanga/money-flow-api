51c2e10cbac9dd220ee451f5d7c6bc96aff584f3
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2025-03-24 22:48:47 -0300

    amo spring

--- src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
@@ -2,64 +2,65 @@ package br.com.fiap.money_flow_api.controller;
 
 
 import br.com.fiap.money_flow_api.model.Category;
+import br.com.fiap.money_flow_api.repository.CategoryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("categories")
 public class CategoryController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
-    private List<Category> repository = new ArrayList<>();
 
-    @GetMapping()
+    @Autowired  // injeção de dependência
+    private CategoryRepository repository;
+
+    @GetMapping
     public List<Category> index() {
-        return repository;
+        return repository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public Category getById(@PathVariable Long id) {
         return getCategory(id); // 200
     }
 
-    @PostMapping()
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Category> create(@RequestBody Category category) {
         log.info("Cadastrando..." + category.getName());
-        repository.add(category);
+        repository.save(category);
         return ResponseEntity.status(201).body(category);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable Long id) {
         log.info("Apagando categoria: " + id);
-        repository.remove(getCategory(id));
+        repository.delete(getCategory(id));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("{id}")
     public Category update(@PathVariable Long id, @RequestBody Category category) {
         log.info("Atualizando categoria " + id + " " + category);
 
-        repository.remove(getCategory(id));
+        getCategory(id);
         category.setId(id);
-        repository.add(category);
+        repository.save(category);
 
         return category;
     }
 
     private Category getCategory(Long id) {
         return repository
-            .stream()
-            .filter(c -> c.getId().equals(id))
-            .findFirst()
+            .findById(id)
             .orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
             );
---
+++ src/main/java/br/com/fiap/money_flow_api/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.money_flow_api.repository;
+
+import br.com.fiap.money_flow_api.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
